e697d13ea7ed33de24682c6f4c94abd32c3f13ff
waa2l <lu2aorange@gmail.com>
2025-12-06 23:39:13 +0200

    Create supabaseClient.js

---
+++ supabaseClient.js
@@ -0,0 +1,2 @@
+const SUPABASE_URL = 'https://dkvefbjgsnhrkjpwprux.supabase.co'; // ضع رابط مشروعك
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRrdmVmYmpnc25ocmtqcHdwcnV4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjUwNTQ5MDksImV4cCI6MjA4MDYzMDkwOX0.fZxEFQc9iUtqX6XEtuuy_XfRIMp9oR3RKmJ84rUzyGw'; // ضع المفتاح العام
